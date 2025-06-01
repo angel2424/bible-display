059ccde35507e042c3c4bbf6a0096aa1981d0556
Angel Rodriguez <angel.rodriguez@cimagroup.com.mx>
2025-06-01 11:04:52 -0500

    Added manifest

--- vite.config.js
+++ vite.config.js
@@ -12,9 +12,32 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     tailwindcss(),
-    VitePWA({ registerType: 'autoUpdate', devOptions: {
-      enabled: true
-    } })
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Bible Display',
+        short_name: 'bible-display',
+        theme_color: '#1e1e1e', // 👈 Theme color for the browser UI
+        background_color: '#ffffff', // 👈 Splash screen background
+        display: 'standalone',
+        start_url: '.',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      },
+      workbox: {
+        // Optional: add custom caching rules
+      }
+    })
   ],
   resolve: {
     alias: {
